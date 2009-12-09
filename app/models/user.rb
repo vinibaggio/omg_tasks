d7db1abdff5e71fd47420d64fa9a8b7f59af5d95
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-08 22:15:31 -0200

    Allowing attachments for photos during creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_attached_file :photo
   devise :authenticatable, :authentication_keys => [:username]
   
-  attr_accessible :username, :password, :password_confirmation
+  attr_accessible :username, :password, :password_confirmation, :photo
   validates_confirmation_of :password, :on => :create
   validates_presence_of :username, :password
 end
