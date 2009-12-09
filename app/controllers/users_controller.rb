d17e463cab8ff9fbc7784b5b326216cf05bbed67
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-08 22:19:40 -0200

    Redirecting to a place that exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < InheritedResources::Base
   actions :create, :new
+  
+  def create
+    create! do |format|
+      format.html { redirect_to root_path }
+    end
+  end
 end
