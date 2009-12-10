2184ace49f668927e262dc30cefb643a571ef15d
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 00:07:05 -0200

    Adding search by username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < InheritedResources::Base
   actions :create, :new, :show
   
+  def show
+    @user = User.find_by_username(params[:id])
+    show!
+  end
+  
   def create
     create! do |format|
       format.html { redirect_to root_path }
