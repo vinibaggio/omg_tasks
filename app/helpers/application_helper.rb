c49e0e7162dd1451a19c1a05e343af017d1eeb60
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 00:48:16 -0200

    Add link for user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,10 @@ module ApplicationHelper
   
   def display_user_bar
     if current_user
-      "logged in as #{current_user.username} - " + link_to("logout", destroy_user_session_path)
+      "logged in as " +
+        link_to(current_user.username, user_path(current_user)) +
+        " - " +
+        link_to("logout", destroy_user_session_path)
     else
       link_to "login", new_user_session_path
     end
