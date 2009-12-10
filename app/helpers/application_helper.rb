873d0f9cbc2697d2c2f9849e7a0f3d75ac9ecce2
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 00:44:01 -0200

    Adding proper method call to verify users login and spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   
   def display_user_bar
-    if @current_user && @current_user.respond_to?(:username)
-      "logged in as #{@current_user.username} - " + link_to("logout", destroy_user_session_path)
+    if current_user
+      "logged in as #{current_user.username} - " + link_to("logout", destroy_user_session_path)
     else
       link_to "login", new_user_session_path
     end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,13 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ApplicationHelper do
   
+  include Devise::TestHelpers
+  
+  before(:all) do 
+    @user = User.new({:username => "vinibaggio"})
+  end
+  
   it "should show user logged in details when user logged in" do
-    assigns[:current_user] = User.new({:username => "vinibaggio"})
+    helper.stub!(:current_user).and_return(@user)
     helper.display_user_bar.include?("logout").should be_true
   end
   
   it "should show user logged in details when user is not logged in" do
-    assigns[:current_user] = nil
+    helper.stub!(:current_user).and_return(nil)
     helper.display_user_bar.include?("login").should be_true
   end
 end
