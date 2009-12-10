5d2a57e0e2029245d98e1cd2517ed441ca6a8a37
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 00:33:45 -0200

    Adding helper method that displays links for login and logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
   
-  def show_logged_user_bar
-    if @current_user
-      
+  def show_user_bar
+    if @current_user && @current_user.respond_to?(:username)
+      "logged in as #{@current_user.username} - " + link_to("logout", destroy_user_session_path)
+    else
+      link_to "login", new_user_session_path
     end
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ApplicationHelper do
+  
   it "should show user logged in details when user logged in" do
-    helper.show_logged_user_bar.should == /vinibaggio/
+    assigns[:current_user] = User.new({:username => "vinibaggio"})
+    helper.show_user_bar.include?("logout").should be_true
   end
   
   it "should show user logged in details when user is not logged in" do
-    helper.show_logged_user_bar.should be_nil
+    assigns[:current_user] = nil
+    helper.show_user_bar.include?("login").should be_true
   end
 end
