4e77ffd4d5a42986b9ba834f4441003e55ad2878
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-09 23:48:47 -0200

    Changing spec to conform to_param and allow accented characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation, :photo
   validates_confirmation_of :password, :on => :create
   validates_presence_of :username, :password
+  validates_format_of :username, :with => /^[\w\d]+$/, :on => :create, :message => "is invalid", :allow_blank => false, :allow_nil => false
+  
+  def to_param
+    username.to_param
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,14 @@ describe User do
   
   should_have_attached_file :photo
   should_validate_confirmation_of :password
-  should_validate_presence_of :username, :password
+  should_validate_presence_of :password
+  should_allow_values_for :username, "vinibaggio", "blablebli1", "bla123bla", :allow_blank => false, :allow_nil => false
+  should_not_allow_values_for :username, "   ", "ola!"
   
   # check how to test devise
+  
+  it "should use username as a parameter to construct urls" do
+    user = User.new({:username => "vinibaggio"})
+    user.to_param.should == "vinibaggio"
+  end
 end
