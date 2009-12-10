2a6e2fdf82ca3d5b4ed01ef9889e23abd2317f57
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 00:34:38 -0200

    Changing method name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
-  def show_user_bar
+  def display_user_bar
     if @current_user && @current_user.respond_to?(:username)
       "logged in as #{@current_user.username} - " + link_to("logout", destroy_user_session_path)
     else
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,11 +4,11 @@ describe ApplicationHelper do
   
   it "should show user logged in details when user logged in" do
     assigns[:current_user] = User.new({:username => "vinibaggio"})
-    helper.show_user_bar.include?("logout").should be_true
+    helper.display_user_bar.include?("logout").should be_true
   end
   
   it "should show user logged in details when user is not logged in" do
     assigns[:current_user] = nil
-    helper.show_user_bar.include?("login").should be_true
+    helper.display_user_bar.include?("login").should be_true
   end
 end
