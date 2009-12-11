73f106272ccac4eeb7ad09c2de1c07d12ef01a85
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 23:53:53 -0200

    Adding method to check if current user is accessing data his own data

--- app/controllers/users/filters.rb
+++ app/controllers/users/filters.rb
@@ -3,8 +3,14 @@ module Users::Filters
     before_filter :find_user_from_session
     defaults options.reverse_merge(:route_prefix => nil)
     
+    include ControllerMethods 
+  end
+  
+  def acts_as_current_user(options={})
+    prepend_before_filter :find_user_from_session, :current_user_only
+    defaults options.reverse_merge(:route_prefix => nil)
+    
     include ControllerMethods
-      
   end
   
   module ControllerMethods
@@ -22,5 +28,14 @@ module Users::Filters
         @current_user ||= current_user
       end
     end
+    
+    def current_user_only
+      unless @current_user.username == params[:user_id]
+        flash[:error] = "You are not allowed here"
+        redirect_to :back
+        false
+      end
+    end
+    
   end
 end
