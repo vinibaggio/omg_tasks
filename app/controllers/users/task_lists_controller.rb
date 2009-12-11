26643fa5d0f35858271f424545ffd71b9012eee7
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-11 00:44:12 -0200

    Submitting task lists with multiple tasks. No add more link yet

--- app/controllers/users/task_lists_controller.rb
+++ app/controllers/users/task_lists_controller.rb
@@ -2,4 +2,15 @@ class Users::TaskListsController < InheritedResources::Base
   acts_as_current_user
   actions :all, :except => [:update]
   
+  def new
+    @task_list = TaskList.new
+    2.times {@task_list.tasks.build}
+  end
+  
+  def create
+    create! do |success, failure|
+      success.html { redirect_to user_task_lists_path(@current_user) }
+    end
+  end
+  
 end
