d97a507d9fb476354ddfcac4dbeaf91c21cd0f60
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-11 00:16:40 -0200

    Fix route prefix

--- app/controllers/users/filters.rb
+++ app/controllers/users/filters.rb
@@ -1,14 +1,19 @@
 module Users::Filters
   def acts_as_user(options={})
     before_filter :find_user_from_session
-    defaults options.reverse_merge(:route_prefix => nil)
+    # Although our namespace is named 'users', things are owned by a single user, so
+    # namespace generated routes by IH should point to 'user'
+    defaults options.reverse_merge(:route_prefix => 'user')
     
     include ControllerMethods 
   end
   
   def acts_as_current_user(options={})
+    # fetch the user first, then verify if the user can access
     prepend_before_filter :find_user_from_session, :current_user_only
-    defaults options.reverse_merge(:route_prefix => nil)
+    # Although our namespace is named 'users', things are owned by a single user, so
+    # namespace generated routes by IH should point to 'user'
+    defaults options.reverse_merge(:route_prefix => 'user')
     
     include ControllerMethods
   end
