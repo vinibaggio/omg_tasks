805de3b5cdf6e09249af2925a2f10c3a6b8b5091
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-10 23:00:25 -0200

    Adding requirements for title and description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
+  
+  validates_presence_of :title, :description
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Task do
   should_belong_to :task_list
+  
+  should_validate_presence_of :title, :description
 end
