9a3a84eef602283eecb20489e62822c1d9958930
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-12-11 22:58:57 -0200

    Implementing helper for requested user

--- app/helpers/users/users_helper.rb
+++ app/helpers/users/users_helper.rb
@@ -1,5 +1,6 @@
 module Users::UsersHelper
   def requested_eql_current?
-    
+    return false if @current_user.nil? or @requested_user.nil?
+    @current_user.eql?(@requested_user)
   end
 end
--- spec/helpers/users/users_helper_spec.rb
+++ spec/helpers/users/users_helper_spec.rb
@@ -1,9 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Users::UsersHelper do
+  
+  before(:all) do
+    @user_a = User.new(:username => "123")
+    @user_b = User.new(:username => "abc")
+  end
+  
   context "when requester is not the same as the logged user" do
     
     it "should tell that requested isn't equal to current" do
+      assigns[:current_user] = @user_a
+      assigns[:requested_user] = @user_b
       helper.requested_eql_current?.should be_false
     end
     
@@ -12,8 +20,22 @@ describe Users::UsersHelper do
   context "when requester is the same as the logged user" do
     
     it "should tell that requested equal to current" do
+      assigns[:current_user] = @user_a
+      assigns[:requested_user] = @user_a
       helper.requested_eql_current?.should be_true
     end
     
+  end
+  
+  it "should tell that requested isn't equal if current user is no one" do
+    assigns[:current_user] = nil
+    assigns[:requested_user] = @user_a
+    helper.requested_eql_current?.should be_false
+  end
+  
+  it "should tell that requested isn't equal if requested user is no one" do
+    assigns[:current_user] = @user_a
+    assigns[:requested_user] = nil
+    helper.requested_eql_current?.should be_false
   end
 end
